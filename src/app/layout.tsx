79999f541f74ffe7ae4ef4b744d94de7c1defc8d
Bezyl Mophat Otieno <bezyl.otieno@griffinglobaltech.com>
2024-08-22 06:48:19 +0300

    main: Refactored project layout and folder structure

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,33 +17,48 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-
   return (
     <>
       <Head>
-	{/* <!-- Favicon --> */}
-	<link rel="shortcut icon" href="assets/images/favicon.ico"/>
-
-	{/* <!-- Google Font --> */}
-	<link rel="preconnect" href="https://fonts.googleapis.com"/>
-	<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous"/>
-
-
+        {/* <!-- Favicon --> */}
+        <link rel="shortcut icon" href="assets/images/favicon.ico" />
+        {/* <!-- Google Font --> */}
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
       </Head>
       <html lang="en" suppressHydrationWarning>
-      
-      <body className={inter.className}>
-        <ThemeProvider attribute="data-bs-theme">
-        {children}
-        </ThemeProvider>
-            <Script strategy="beforeInteractive" src="assets/vendor/bootstrap/dist/js/bootstrap.bundle.min.js"></Script>
-            <Script strategy="beforeInteractive" src="assets/vendor/tiny-slider/tiny-slider.js"></Script>
-            <Script strategy="beforeInteractive" src="assets/vendor/glightbox/js/glightbox.js"></Script>
-            <Script strategy="beforeInteractive" src="assets/vendor/flatpickr/js/flatpickr.min.js"></Script>
-            <Script strategy="beforeInteractive" src="assets/vendor/choices/js/choices.min.js"></Script>
-            <Script strategy="beforeInteractive" src="assets/js/functions.js"></Script>
-            {/* <Script strategy="beforeInteractive" src="assets/js/theme.js"></Script> */}
-      </body>
+        <body className={inter.className}>
+          <ThemeProvider attribute="data-bs-theme">{children}</ThemeProvider>
+          <Script
+            strategy="beforeInteractive"
+            src="assets/vendor/bootstrap/dist/js/bootstrap.bundle.min.js"
+          ></Script>
+          <Script
+            strategy="beforeInteractive"
+            src="assets/vendor/tiny-slider/tiny-slider.js"
+          ></Script>
+          <Script
+            strategy="beforeInteractive"
+            src="assets/vendor/glightbox/js/glightbox.js"
+          ></Script>
+          <Script
+            strategy="beforeInteractive"
+            src="assets/vendor/flatpickr/js/flatpickr.min.js"
+          ></Script>
+          <Script
+            strategy="beforeInteractive"
+            src="assets/vendor/choices/js/choices.min.js"
+          ></Script>
+          <Script
+            strategy="beforeInteractive"
+            src="assets/js/functions.js"
+          ></Script>
+          {/* <Script strategy="beforeInteractive" src="assets/js/theme.js"></Script> */}
+        </body>
       </html>
     </>
   );
